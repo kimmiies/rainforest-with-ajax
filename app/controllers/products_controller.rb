495990786c6f98f492ba3960950a36a18f1e81ef
Kimberly Oleiro <koleiro@gmail.com>
2016-04-28 16:04:46 -0400

    Updated products controller to remove the request.xhr? because the respond_to helper takes care of this for us. It will render the partial via the index.js.erb view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,10 +8,6 @@ class ProductsController < ApplicationController
         Product.all
       end
 
-      if request.xhr?
-        render @products
-      end
-
       respond_to do |format|
         format.html
         format.js
